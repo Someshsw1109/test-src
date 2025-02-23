116d90a18de54d6414cd343ad39634c78589da96
Somesh Raj <someshraj78569@gmail.com>
2025-02-23 16:45:01 +0000

    Assistant checkpoint: Update categories to match product inventory
    Assistant generated file changes:
    - src/components/category/Category.jsx: Update categories to match products
    
    ---
    
    User prompt:
    
    Now avi jo tumne all products me jitne v products the un sb ko category wised tum add kar ke iss code ko update karo (category.jsx)

--- src/components/category/Category.jsx
+++ src/components/category/Category.jsx
@@ -1,61 +1,58 @@
+
 import { useNavigate } from "react-router-dom";
 
 // category 
 const category = [
     {
-        image: 'https://cdn-icons-png.flaticon.com/256/4359/4359963.png',
-        name: 'Entertainment'
+        image: 'https://cdn-icons-png.flaticon.com/256/3081/3081648.png',
+        name: 'School Supplies'
     },
     {
-        image: 'https://cdn-icons-png.flaticon.com/256/12114/12114279.png',
-        name: 'Technology'
+        image: 'https://cdn-icons-png.flaticon.com/256/2503/2503536.png',
+        name: 'Uniforms'
     },
     {
-        image: 'https://cdn-icons-png.flaticon.com/256/8174/8174424.png',
-        name: 'Dresses'
+        image: 'https://cdn-icons-png.flaticon.com/256/3659/3659899.png',
+        name: 'Electronics'
     },
     {
-        image: 'https://cdn-icons-png.flaticon.com/256/7648/7648246.png',
-        name: 'Electronics Items'
+        image: 'https://cdn-icons-png.flaticon.com/256/2942/2942544.png',
+        name: 'Bags'
     },
     {
-        image: 'https://cdn-icons-png.flaticon.com/256/12142/12142416.png',
-        name: 'Laptop'
+        image: 'https://cdn-icons-png.flaticon.com/256/3321/3321681.png',
+        name: 'Sports'
     },
     {
-        image: 'https://cdn-icons-png.flaticon.com/256/10686/10686553.png',
-        name: 'Shoes'
+        image: 'https://cdn-icons-png.flaticon.com/256/3262/3262308.png',
+        name: 'Art Supplies'
     },
     {
-        image: 'https://cdn-icons-png.flaticon.com/256/11946/11946316.png',
-        name: 'Stationary'
+        image: 'https://cdn-icons-png.flaticon.com/256/8062/8062290.png',
+        name: 'Books'
+    },
+    {
+        image: 'https://cdn-icons-png.flaticon.com/256/3771/3771436.png',
+        name: 'Music'
     }
 ]
 
 const Category = () => {
-    // naviaget 
     const navigate = useNavigate();
     return (
         <div>
             <div className="flex flex-col mt-5">
-                {/* main 1 */}
-                <div className="flex overflow-x-scroll lg:justify-center  hide-scroll-bar">
-                    {/* main 2  */}
-                    <div className="flex ">
-                        {/* category  */}
+                <div className="flex overflow-x-scroll lg:justify-center hide-scroll-bar">
+                    <div className="flex">
                         {category.map((item, index) => {
                             return (
                                 <div key={index} className="px-3 lg:px-10">
-                                    {/* Image  */}
-                                    <div onClick={() => navigate(`/category/${item.name}`)} className=" w-16 h-16 lg:w-24 lg:h-24 max-w-xs rounded-full  bg-pink-500 transition-all hover:bg-pink-400 cursor-pointer mb-1 " >
+                                    <div onClick={() => navigate(`/category/${item.name}`)} className="w-16 h-16 lg:w-24 lg:h-24 max-w-xs rounded-full bg-pink-500 transition-all hover:bg-pink-400 cursor-pointer mb-1">
                                         <div className="flex justify-center mb-12">
-                                            {/* Image tag  */}
-                                            <img src={item.image} alt="img" />
+                                            <img src={item.image} alt={item.name} className="w-12 h-12 lg:w-16 lg:h-16 object-contain mt-2 lg:mt-4" />
                                         </div>
                                     </div>
-
-                                    {/* Name Text  */}
-                                    <h1 className=' text-sm lg:text-lg text-center font-medium title-font first-letter:uppercase '>{item.name}</h1>
+                                    <h1 className="text-sm lg:text-lg text-center font-medium title-font first-letter:uppercase">{item.name}</h1>
                                 </div>
                             )
                         })}
@@ -63,7 +60,6 @@ const Category = () => {
                 </div>
             </div>
 
-            {/* style  */}
             <style dangerouslySetInnerHTML={{ __html: "\n.hide-scroll-bar {\n  -ms-overflow-style: none;\n  scrollbar-width: none;\n}\n.hide-scroll-bar::-webkit-scrollbar {\n  display: none;\n}\n" }} />
         </div>
     );
